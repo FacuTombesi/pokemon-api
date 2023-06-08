4e7138ca7a986ac53581ab1eeb3111ae58a4da2b
FacuTombesi <facundomtombesi@gmail.com>
2023-06-08 01:13:10 -0300

    types/controllers fix

--- api/src/controllers/typeControllers.js
+++ api/src/controllers/typeControllers.js
@@ -11,9 +11,9 @@ const getAllTypes = async () => {
     for (let type of types) {
         let findType = await Type.findOne({ where: { name: type.name } }); // Busco si ya tengo el tipo de pokemon para evitar duplicarlo
         if (findType) {
-            typesNames.push(findType);
+            typesNames.push(findType); // Si lo encuentro, lo pusheo a mi nuevo array
         }   else {
-            const newType = await Type.create({
+            const newType = await Type.create({ // De lo contrario, creo un nuevo tipo y lo pusheo
                 name: type.name
             });
             typesNames.push(newType);
