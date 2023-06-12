0d5b062b98add8d385fe34b58e9d203fd3b68224
FacuTombesi <facundomtombesi@gmail.com>
2023-06-12 16:50:08 -0300

    index/app/components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+import PokemonDetail from "./components/PokemonDetail";
+import CreatePokemon from "./components/CreatePokemon";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
+    <div>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Home} />
+        <Route path="/pokemons/:id" component={PokemonDetail} />
+        <Route path="/pokemons/create" component={CreatePokemon} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
